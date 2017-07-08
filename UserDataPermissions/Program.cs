c3cc06ae79dce802014754bb1f55394a25256f33
Daniel Schausberger <d.schausberger@bdk.local>
2017-07-08 11:33:40 +0200

    first commit

---
+++ UserDataPermissions/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDataPermissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Permissions according to https://technet.microsoft.com/en-us/library/jj649078(v=ws.11).aspx with change of Administratos having full access
+            //Child directories must be equal to the user names!
+
+            //optional: start from a custom userdir folder. useful for debugging
+            bool startFromReached = Settings.Default.startFrom == "";
+            String settingsFile = "UserDataPermissions.exe.config";
+
+            if (!File.Exists(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\" + settingsFile))
+            {
+                Log(settingsFile + " does not exist in exe dir -> exiting.");
+                Environment.ExitCode = -30;
+                return;
+            }
+
+            //Parent (UserData)
+            Log(Settings.Default.pathUserData + ": Setting parent permissions");
+            DirectoryInfo parent = new DirectoryInfo(Settings.Default.pathUserData);
+
+            if (!parent.Exists)
+            {
+                Log("Dir does not exist -> exiting.");
+                Environment.ExitCode = -10;
+                return;
+            }
+
+            if (Settings.Default.domain == "")
+            {
+                Log("Please set your domain name in config file -> exiting.");
+                Environment.ExitCode = -40;
+                return;
+            }
+
+            DirectorySecurity sec;
+
+            if (!Settings.Default.skipParentPermissions)
+            {
+                sec = new DirectorySecurity(parent.FullName, AccessControlSections.All);
+                sec.SetAccessRuleProtection(true, false); //ignore inherited
+                RemoveExplicitSecurity(sec);
+                try
+                {
+                    foreach (String user in Settings.Default.fullAccessUsers)
+                        sec.AddAccessRule(new FileSystemAccessRule(user, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                    sec.AddAccessRule(new FileSystemAccessRule(Settings.Default.defaultGroup, FileSystemRights.ListDirectory | FileSystemRights.CreateDirectories | FileSystemRights.ReadAttributes | FileSystemRights.ReadExtendedAttributes | FileSystemRights.ReadPermissions, InheritanceFlags.None, PropagationFlags.None, AccessControlType.Allow));
+                    sec.AddAccessRule(new FileSystemAccessRule(Settings.Default.ownersGroup, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.InheritOnly, AccessControlType.Allow));
+                    parent.SetAccessControl(sec);
+                }
+                catch (IdentityNotMappedException ex)
+                {
+                    Log("Did not find users or groups in your system -> exiting. Maybe your OS uses a different language, please adjust user or group names.\r\nMessage details: " + ex.Message);
+                    Environment.ExitCode = -20;
+                    return;
+                }
+            }
+            else
+                Log("Skipping parent permissions.");
+
+
+            Log("Giving executing user restore privileges");
+            UnmanagedCode.GiveRestorePrivilege(); //otherwise invalidoperationexception at setowner()
+
+            foreach(DirectoryInfo userdir in parent.GetDirectories())
+            {
+                Log(userdir.FullName);
+
+                if (Settings.Default.exceptions.Contains(userdir.Name))
+                {
+                    Log("Skipping " + userdir.FullName);
+                    continue;
+                }
+
+                if (userdir.Name == Settings.Default.startFrom)
+                    startFromReached = true;
+
+                if (!startFromReached)
+                {
+                    Log("Skipping " + userdir.Name + " (looking for " + Settings.Default.startFrom + " to start from)");
+                    continue;
+                }
+
+                IdentityReference user = new NTAccount(Settings.Default.domain, userdir.Name);
+               
+                try
+                {
+                    user.Translate(typeof(SecurityIdentifier)); //only necessary to check if user exists in domain
+                }
+                catch(IdentityNotMappedException)
+                {
+                    Log(" User " + userdir.Name + " not found in domain " + Settings.Default.domain);
+                    continue;
+                }
+
+                sec = new DirectorySecurity(userdir.FullName, AccessControlSections.All);
+                RemoveExplicitSecurity(sec);
+                sec.SetAccessRuleProtection(false, false); //inherit
+                //optional: Set file access right explicitly; not necessary because of creator-owner permission set in parent
+                //sec.AddAccessRule(new FileSystemAccessRule(userdir.Name, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                sec.SetOwner(user);
+                userdir.SetAccessControl(sec);
+
+                //Subdirs
+                Log("  Processing subdirs");
+                foreach (DirectoryInfo usersubdir in userdir.GetDirectories("*.*", SearchOption.AllDirectories))
+                {
+                    if (Settings.Default.debug)
+                        Log("    " + usersubdir.FullName);
+
+                    sec = new DirectorySecurity(usersubdir.FullName, AccessControlSections.All);
+                    RemoveExplicitSecurity(sec);
+                    sec.SetAccessRuleProtection(false, false);
+                    //sec.AddAccessRule(new FileSystemAccessRule(usersubdir.Name, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                    sec.SetOwner(user);
+                    usersubdir.SetAccessControl(sec);
+                }
+
+                //Subfiles
+                Log("  Processing subfiles");
+                foreach(FileInfo userfile in userdir.GetFiles("*.*", SearchOption.AllDirectories))
+                {
+                    if (Settings.Default.debug)
+                        Log("    " + userfile.FullName);
+
+                    FileSecurity filesec;
+
+                    try
+                    {
+                        filesec = new FileSecurity(userfile.FullName, AccessControlSections.All);
+                        RemoveExplicitSecurity(filesec);
+                        filesec.SetAccessRuleProtection(false, false);
+                        //sec.AddAccessRule(new FileSystemAccessRule(userfile.Name, FileSystemRights.FullControl, InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit, PropagationFlags.None, AccessControlType.Allow));
+                        filesec.SetOwner(user);
+                        userfile.SetAccessControl(filesec);
+                    }
+                    catch (ArgumentException)
+                    {
+                        Log("Error processing file (path might be too long): " + userfile.FullName);
+                        continue;
+                    }
+
+                }
+
+            }
+            Log("Done");
+
+        }
+
+        private static void RemoveExplicitSecurity(DirectorySecurity directorySecurity)
+        {
+            AuthorizationRuleCollection rules = directorySecurity.GetAccessRules(true, false, typeof(System.Security.Principal.NTAccount));
+            foreach (FileSystemAccessRule rule in rules)
+                directorySecurity.RemoveAccessRule(rule);
+        }
+
+        private static void RemoveExplicitSecurity(FileSecurity fileSecurity)
+        {
+            AuthorizationRuleCollection rules = fileSecurity.GetAccessRules(true, false, typeof(System.Security.Principal.NTAccount));
+            foreach (FileSystemAccessRule rule in rules)
+                fileSecurity.RemoveAccessRule(rule);
+        }
+
+        private static void Log(String text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ UserDataPermissions/UnmanagedCode.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDataPermissions
+{
+    /// <summary>
+    /// from http://blog.salamandersoft.co.uk/index.php/2009/10/setting-the-owner-of-files-and-directories-in-c/
+    /// </summary>
+    sealed class UnmanagedCode
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        // Use this signature if you do not want the previous state
+        [DllImport("advapi32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AdjustTokenPrivileges(IntPtr tokenHandle,
+            [MarshalAs(UnmanagedType.Bool)]bool disableAllPrivileges,
+            ref TOKEN_PRIVILEGES newState,
+            UInt32 bufferLength,
+            IntPtr previousState,
+            IntPtr returnLength);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", ExactSpelling = true, SetLastError = true)]
+        static extern bool OpenProcessToken
+            (IntPtr processHandle, int desiredAccess, ref IntPtr phtok);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool LookupPrivilegeValue
+                (string host, string name, ref LUID lpLuid);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        struct TOKEN_PRIVILEGES
+        {
+            public UInt32 PrivilegeCount;
+            public LUID Luid;
+            public UInt32 Attributes;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LUID
+        {
+            public uint LowPart;
+            public int HighPart;
+        }
+
+        const int SE_PRIVILEGE_ENABLED = 0x00000002;
+        const int TOKEN_QUERY = 0x00000008;
+        const int TOKEN_ADJUST_PRIVILEGES = 0x00000020;
+        //http://msdn.microsoft.com/en-us/library/bb530716(VS.85).aspx
+        const string SE_RESTORE_PRIVILEGE = "SeRestorePrivilege";
+
+        public static void GiveRestorePrivilege()
+        {
+            TOKEN_PRIVILEGES tokenPrivileges;
+            tokenPrivileges.PrivilegeCount = 1;
+            tokenPrivileges.Luid = new LUID();
+            tokenPrivileges.Attributes = SE_PRIVILEGE_ENABLED;
+
+            IntPtr tokenHandle = RetrieveProcessToken();
+
+            try
+            {
+                bool success = LookupPrivilegeValue
+                            (null, SE_RESTORE_PRIVILEGE, ref tokenPrivileges.Luid);
+                if (success == false)
+                {
+                    int lastError = Marshal.GetLastWin32Error();
+                    throw new Exception(
+                        string.Format("Could not find privilege {0}. Error {1}",
+                                            SE_RESTORE_PRIVILEGE, lastError));
+                }
+
+                success = AdjustTokenPrivileges(
+                                                    tokenHandle, false,
+                                                    ref tokenPrivileges, 0,
+                                                    IntPtr.Zero, IntPtr.Zero);
+                if (success == false)
+                {
+                    int lastError = Marshal.GetLastWin32Error();
+                    throw new Exception(
+                        string.Format("Could not assign privilege {0}. Error {1}",
+                                        SE_RESTORE_PRIVILEGE, lastError));
+                }
+            }
+            finally
+            {
+                CloseHandle(tokenHandle);
+            }
+
+        }
+
+        
+
+        static IntPtr RetrieveProcessToken()
+        {
+            IntPtr processHandle = GetCurrentProcess();
+            IntPtr tokenHandle = IntPtr.Zero;
+            bool success = OpenProcessToken(processHandle,
+                                            TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
+                                            ref tokenHandle);
+            if (success == false)
+            {
+                int lastError = Marshal.GetLastWin32Error();
+                throw new Exception(
+                    string.Format("Could not retrieve process token. Error {0}",
+                                        lastError));
+            }
+            return tokenHandle;
+        }
+    }
+
+}
